05afbe38ffd515e6c7aeb1fc4a809611d671a923
guoyongzheng <guoyongzheng@agilestar.cn>
2025-08-27 19:11:32 +0800

    init

---
+++ src/main/java/com/open/capacity/itextpdfdemo/ItextPdfDemoApplication.java
@@ -0,0 +1,13 @@
+package com.open.capacity.itextpdfdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItextPdfDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItextPdfDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/open/capacity/itextpdfdemo/ItextPdfDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.open.capacity.itextpdfdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItextPdfDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
